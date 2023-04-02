c12ffd24620654046200b9bd0ca99a6494c19e73
darismathew92 <darismathew922000@gmail.com>
2023-04-02 03:27:26 +0100

    buton remove

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -205,13 +205,6 @@ const Feed = () => {
                   >
                     Create Post
                   </button>
-
-                  <button
-                    className="bg-[#0095F6] py-2 px-4 text-white active:scale-95 transform transition  disabled:bg-opacity-50 select-none cursor-pointer disabled:scale-100 rounded text-xl font-semibold"
-                    onClick={handleUploadPost}
-                  >
-                    Create NFT
-                  </button>
                 </div>
               </div>
             )}
